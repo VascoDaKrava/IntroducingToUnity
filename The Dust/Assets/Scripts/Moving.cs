50cb7e33a5c2efb7896780eba02df8f5ee816b0b
Vasco-PC <kravchuk.vasilii@yandex.ru>
2021-08-16 17:59:29 +0300

    Environment and script
    Added exit-hole.
    Corrected structure of moving-script.
    Added mouse-view.
    Added temp directed cube to prefabs.

--- The Dust/Assets/Scripts/Moving.cs
+++ The Dust/Assets/Scripts/Moving.cs
@@ -4,27 +4,66 @@ using UnityEngine;
 
 public class Moving : MonoBehaviour
 {
-    private Vector2 _input;
-    private Vector3 _position;
-    private Rigidbody _rigidbody;
+    [SerializeField] private Camera _camera;
+
+    private Vector2 _inputMoveDirection;
+    private Vector3 _inputLookDirection;
+    private Vector3 _playerPosition;
+    private Rigidbody _playerRigidbody;
+    private Transform _playerTransform;
+    private Transform _cameraTransform;
+    private float _speed = 0.1f;
 
     // Start is called before the first frame update
     void Start()
     {
-        _rigidbody = GetComponent<Rigidbody>();
-        _position = _rigidbody.position;
+        _playerRigidbody = GetComponent<Rigidbody>();
+        _playerTransform = GetComponent<Transform>();
+        _cameraTransform = _camera.transform;
+        _inputLookDirection = _cameraTransform.eulerAngles;
+        _playerPosition = _playerTransform.position;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        _input.x = Input.GetAxis("Horizontal");
-        _input.y = Input.GetAxis("Vertical");
+        getInput();
+        cameraRotate();
+        playerMoving();
+    }
 
-        //_rigidbody.MovePosition(_rigidbody.position + _input);
-        _position.x += _input.x;
-        _position.y = 0;
-        _position.z += _input.y;
-        _rigidbody.MovePosition(_position);
+    /// <summary>
+    /// Get inputs
+    /// </summary>
+    private void getInput()
+    {
+        _inputMoveDirection.x = Input.GetAxis("Horizontal");
+        _inputMoveDirection.y = Input.GetAxis("Vertical");
+        _inputMoveDirection.Normalize();
+
+        _inputLookDirection.x += Input.GetAxis("Mouse X");
+        _inputLookDirection.y -= Input.GetAxis("Mouse Y");
+    }
+
+    /// <summary>
+    /// Change cameras angles
+    /// </summary>
+    private void cameraRotate()
+    {
+        _cameraTransform.rotation = Quaternion.Euler(_inputLookDirection.y, _inputLookDirection.x, 0f);
+    }
+
+    /// <summary>
+    /// Move and rotate player
+    /// </summary>
+    private void playerMoving()
+    {
+        _playerPosition.x += _inputMoveDirection.x;
+        _playerPosition.z += _inputMoveDirection.y;
+        _playerTransform.rotation = Quaternion.Euler(0f, _inputLookDirection.x, 0f);
+
+        //_playerTransform.forward.;
+        _playerRigidbody.MovePosition(_playerPosition * _speed);
     }
 }
