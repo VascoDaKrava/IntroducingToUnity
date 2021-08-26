a382d64d01dcb7fa39b7bc6ae565747fcffcf18c
Vasco-PC <kravchuk.vasilii@yandex.ru>
2021-08-27 01:00:40 +0300

    Nav. Path
    Patrol working

--- The Dust/Assets/Scripts/NavMoving.cs
+++ The Dust/Assets/Scripts/NavMoving.cs
@@ -5,19 +5,37 @@ using UnityEngine.AI;
 
 public class NavMoving : MonoBehaviour
 {
-    [SerializeField] Transform _wayPoint;
-    [SerializeField] NavMeshAgent _agent;
+    private Stack<Vector3> _wayPointsStack;
+    private Stack<Vector3> _wayPointsStack1;
+    [SerializeField] private Transform _target;
+    private NavMeshAgent _agent;
 
     // Start is called before the first frame update
     void Start()
     {
+        _wayPointsStack = new Stack<Vector3>(GameObject.FindGameObjectWithTag("GlobalScript").GetComponent<Storage>().NordWay);
         _agent = GetComponent<NavMeshAgent>();
-        _agent.SetDestination(new Vector3(0, 0, 0));
+        //_agent.SetDestination(_target.position);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!_agent.hasPath)
+            NextPoint();
+    }
+
+    private void NextPoint()
+    {
+        _agent.SetDestination(_wayPointsStack.Peek());
+        Debug.Log("_wayPointsStack.Peek() = " + _wayPointsStack.Peek());
+        foreach (Vector3 item in _wayPointsStack)
+        {
+            Debug.Log("item = " + item);
+        }
+        Vector3 cur = _wayPointsStack.Pop();
+        //_wayPointsStack.Push(cur);
         
+
     }
 }
--- The Dust/Assets/Scripts/Storage.cs
+++ The Dust/Assets/Scripts/Storage.cs
@@ -5,16 +5,48 @@ using UnityEngine;
 
 public class Storage : MonoBehaviour
 {
+    #region For trigger-colliders
+
     // Colliders, that are triggers. For example, they are can not get damaged.
     private List<int> _triggerColliderList = new List<int>();
+    public List<int> TriggerColliderList { get { return _triggerColliderList; } }
+
+    #endregion
+
+    #region For Navigation
+
+    private Stack<Vector3> _nordWay = new Stack<Vector3>();
+    private Stack<Vector3> _undergroundWay = new Stack<Vector3>();
+    private Stack<Vector3> _sandWay = new Stack<Vector3>();
+
+    public Stack<Vector3> NordWay { get { return _nordWay; } }
+    public Stack<Vector3> UndeegroundWay { get { return _undergroundWay; } }
+    public Stack<Vector3> SandWay { get { return _sandWay; } }
+
+    #endregion
+
+    #region Statics for Tags
 
     private static string _playerTag = "Player";
     private static string _platformTag = "QuestPlatform";
+    private static string _nordWayPointsTag = "NordWayPoints";
+    private static string _undergroundWayPointsTag = "UndergroundWayPoints";
+    private static string _sandWayPointsTag = "SandWayPoints";
 
     public static string PlayerTag { get { return _playerTag; } }
     public static string PlatformTag { get { return _platformTag; } }
 
-    public List<int> TriggerColliderList { get { return _triggerColliderList; } }
+    #endregion
+
+    private void Start()
+    {
+        foreach (GameObject item in GameObject.FindGameObjectsWithTag(_nordWayPointsTag))
+        {
+            _nordWay.Push(item.transform.position);
+        }
+    }
+
+    #region Static methods
 
     /// <summary>
     /// Translate X-rotation angle of transform to humanity-like variant (0-360 degree)
@@ -58,4 +90,6 @@ public class Storage : MonoBehaviour
     {
         return name;
     }
+
+    #endregion
 }
