5c43eda7255715cd565a23329ac8b0e64ccad4fe
Vasco-PC <kravchuk.vasilii@yandex.ru>
2021-09-07 18:23:37 +0300

    UI
    Started work on UI

---
+++ The Dust/Assets/Scripts/UI.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour
+{
+    private int _health;
+    private int _armor;
+    private int _bullets;
+
+    private string _healthTxt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //_healthTxt = 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
