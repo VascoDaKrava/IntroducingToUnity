6482cf19cfa52d04b43ddef7d7cff3388fd72cfc
Vasco-PC <kravchuk.vasilii@yandex.ru>
2021-09-10 18:25:32 +0300

    FX
    Added particle system to grenade

--- The Dust/Assets/Scripts/Weapons/ExplosionController.cs
+++ The Dust/Assets/Scripts/Weapons/ExplosionController.cs
@@ -18,6 +18,7 @@ public class ExplosionController : MonoBehaviour
     private string _boomMethodName = "MakeExplosion";
 
     private AudioSource _audioSource;
+    private ParticleSystem _particleSystem;
     private float _timeForFX = 2.5f;// Length of Audio to play, before object was destroyed
 
     /// <summary>
@@ -92,11 +93,13 @@ public class ExplosionController : MonoBehaviour
         }
         Storage.ToLog(this, Storage.GetCallerName(), "B O O M!");
         _audioSource.Play();
+        _particleSystem.Play();
         Destroy(_bomb, _timeForFX);
     }
 
     private void Start()
     {
         _audioSource = Storage.FindTransformInChildrenWithTag(gameObject, Storage.AudioSourceTag).GetComponent<AudioSource>();
+        _particleSystem = Storage.FindTransformInChildrenWithTag(gameObject, "ParticleSystem").GetComponent<ParticleSystem>();
     }
 }
